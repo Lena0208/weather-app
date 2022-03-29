86e1e43e3caa13b9f35e86428f6ed4514ed8123c
Lena Berger <l.berger@alexander-buerkle.de>
2022-03-29 11:48:42 +0200

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,27 +4,29 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project is {""}
-        <a
-          href="https://github.com/Lena0208/weatherApp-sheCodesPlus"
-          target="_blank"
-          rel="noreferrer"
-          className="gitHubLink"
-        >
-          open-source coded
-        </a>
-        {""} by {""}
-        <a
-          href="https://www.linkedin.com/in/lena-berger-316849197/"
-          target="_blank"
-          rel="noreferrer"
-          id="footerName"
-        >
-          Lena Berger
-        </a>
-      </footer>
+      <div className="container">
+        Weather App
+        <footer>
+          This project is {""}
+          <a
+            href="https://github.com/Lena0208/weatherApp-sheCodesPlus"
+            target="_blank"
+            rel="noreferrer"
+            className="gitHubLink"
+          >
+            open-sourced on GitHub
+          </a>
+          {""} by {""}
+          <a
+            href="https://www.linkedin.com/in/lena-berger-316849197/"
+            target="_blank"
+            rel="noreferrer"
+            id="footerName"
+          >
+            Lena Berger
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
