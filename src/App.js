6c89357542141a5a1910749f26df8db5fe015f48
Lena Berger <l.berger@alexander-buerkle.de>
2022-03-29 11:37:30 +0200

    Added the footer

--- src/App.js
+++ src/App.js
@@ -2,5 +2,29 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project is {""}
+        <a
+          href="https://github.com/Lena0208/weatherApp-sheCodesPlus"
+          target="_blank"
+          rel="noreferrer"
+          className="gitHubLink"
+        >
+          open-source coded
+        </a>
+        {""} by {""}
+        <a
+          href="https://www.linkedin.com/in/lena-berger-316849197/"
+          target="_blank"
+          rel="noreferrer"
+          id="footerName"
+        >
+          Lena Berger
+        </a>
+      </footer>
+    </div>
+  );
 }
